f5fd2388e0f9bd1f00bac0e12e71c6b567ca802c
David Petersson <da6133pe-s@student.lu.se>
2021-12-02 01:59:42 +0100

    Nästan fungerande quiz prototyp

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,98 @@
+const testQuestions = [
+    {
+        question: "Vem är bäst i världen?",
+        a: "Dave the slave",
+        b: "David Petersson",
+        c: "David Fetersson",
+        d: "Who's that guuuuy",
+        correct: "a",
+    },
+    {
+    
+        question: "Hur mycket av den här designen är baxad?",
+        a: "Eeh det behöver vi inte tänka på",
+        b: "Inte så viktigt",
+        c: "Vet du hur många löss det finns i världen?",
+        d: "blablabla",
+        correct: "b",
+    }
+]
+
+function qS(selector) {
+    return document.querySelector(selector);
+}
+
+function qSA(selector) {
+    return document.querySelectorAll(selector);
+}
+
+function eId(id) {
+    return document.getElementById(id);
+}
+
+
+const quiz = eId('quiz');
+const submit = eId('submit');
+let questionElem = eId('question');
+let answerElems = qSA('.answer');
+let aText = eId('a_text');
+let bText = eId('b_text');
+let cText = eId('c_text');
+let dText = eId('d_text');
+
+let scoreCount = 0;
+let questionCounter = 0;
+
+startQuiz();
+
+function startQuiz(){
+
+    deselectAnswers();
+
+    let currentData = testQuestions[questionCounter];
+    questionElem.innerText = currentData.question;
+    aText.innerText = currentData.a;
+    bText.innerText = currentData.b;
+    cText.innerText = currentData.c;
+    dText.innerText = currentData.d;
+}
+
+function deselectAnswers(){
+    answerElems.forEach(element => element.checked = false);
+}
+
+function selected(){
+    let answer;
+
+    answerElems.forEach(element => {
+        if(element.checked) {
+            answer = element;
+        }
+    })
+    return answer;
+}
+
+submit.addEventListener('click', () => {
+    const answer = selected();
+
+    if(answer == question.correct){
+        scoreCount++;
+    }
+
+    questionCounter++;
+
+    if(questionCounter < testQuestions.length){
+        startQuiz();
+    } else {
+        quiz.innerHTML = `
+        <h2>You answered ${scoreCount}/${testQuestions.length} questions correctly</h2>
+
+        <button onclick="location.reload()">Reload</button>
+        `
+    }
+})
+
+
 let mini = true;
 // om sidebaren är minimerad och vi drar musen över den så vill vi att den skall dras ut, när musen inte längre är över så
 // dras den in igen.
@@ -5,12 +100,10 @@ function toggleSidebar() {
     if (mini){
         console.log("opening sidebar");
         document.getElementById("mySidebar").style.width = "250px";
-        document.querySelector("main").style.marginLeft = "250px";
         mini = false;
     } else {
         console.log("closing sidebar");
         document.getElementById("mySidebar").style.width = "85px";
-        document.querySelector("main").style.marginLeft = "85px";
         mini = true;
     }
 }
@@ -23,42 +116,3 @@ function qSA(selector) {
     return document.querySelectorAll(selector);
 }
 
-let remainingGuesses = [], guess = {}, scoreCount = 0, firstGuess = true, subjects = [], firstTry = 0, incorrectGuesses = 0, loading = false;
-const testQuestions = [
-    {
-        question: "Who is Einstein",
-        answers: {
-            a: "Lars Göran",
-            b: "Yo mama",
-            c: "Albert Einsten"
-        },
-        question: "Är David coolast i gruppen?",
-        answers: {
-            a: "Ja",
-            b: "yup",
-            c: "Kan du ge dig fan på"
-        }
-
-    }
-];
-
-qS("#corGuess").innerHTML = `<span>First Try: ${firstTry}</span>`
-qS("#failGuess").innerHTML = `<span>Failed Guesses: ${incorrectGuesses}</span>`
-
-function updateScore(){
-    qS("#score").innerHTML = `<span>Score: ${scoreCount}</span>`
-} 
-
-updateScore();
-startWithAllSubjects();
-
-function startWithAllSubjects() {
-    remainingGuesses = [];
-   
-    
-}
-
-
-function addAnswerListeners() {
-
-}
